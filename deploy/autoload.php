305918633eea820e48250ef13413edacc2294f26
Owen Wolf <owen@wolfslair.cc>
2026-05-06 10:47:58 +0200

    chore: remove unused PHPMailer deploy remnants

--- deploy/autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/PHPMailer/src/Exception.php';
-require_once __DIR__ . '/PHPMailer/src/SMTP.php';
-require_once __DIR__ . '/PHPMailer/src/PHPMailer.php';
